b2eb039fe7b45c0c6af6099efdf692b65f362512
thebcitguy <wow1@my.bcit.ca>
2021-04-21 12:42:52 +0800

    add return and HomeScreen constants

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -15,49 +15,54 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Fontisto from 'react-native-vector-icons/Fontisto';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
+const HomeScreen = ({navigation}) => {
+    const theme = useTheme();
 
-<View style={styles.cardsWrapper}>
-        <button>Post A Job`</button>
-        
-        <Text
-          style={{
-            alignSelf: 'center',
-            fontSize: 18,
-            fontWeight: 'bold',
-            color: '#333',
-          }}>
-          MY POSTED JOBS
-        </Text>
-    <View style={styles.card}>
-          <View style={styles.cardImgWrapper}>
-            <Image
-              source={require('../assets/car1.png')}
-              resizeMode="cover"
-              style={styles.cardImg}
-            />
-            <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-          </View>
 
-          <View style={styles.cardImgWrapper}>
-            <Image
-              source={require('../assets/car2.png')}
-              resizeMode="cover"
-              style={styles.cardImg}
-            />
-            <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-          </View>
+return (
+    <View style={styles.cardsWrapper}>
+            <button>Post A Job`</button>
 
-          <View style={styles.cardImgWrapper}>
-            <Image
-              source={require('../assets/car3.png')}
-              resizeMode="cover"
-              style={styles.cardImg}
-            />
-            <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-          </View>
-         
+            <Text
+            style={{
+                alignSelf: 'center',
+                fontSize: 18,
+                fontWeight: 'bold',
+                color: '#333',
+            }}>
+            MY POSTED JOBS
+            </Text>
+        <View style={styles.card}>
+            <View style={styles.cardImgWrapper}>
+                <Image
+                source={require('../assets/car1.png')}
+                resizeMode="cover"
+                style={styles.cardImg}
+                />
+                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
+            </View>
+
+            <View style={styles.cardImgWrapper}>
+                <Image
+                source={require('../assets/car2.png')}
+                resizeMode="cover"
+                style={styles.cardImg}
+                />
+                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
+            </View>
+
+            <View style={styles.cardImgWrapper}>
+                <Image
+                source={require('../assets/car3.png')}
+                resizeMode="cover"
+                style={styles.cardImg}
+                />
+                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
+            </View>
+            
+        </View>
     </View>
-</View>
-
+    )
+};
 
 export default HomeScreen;
