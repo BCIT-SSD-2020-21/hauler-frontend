5970b42504717487de3e885af93dbd13f7bc9725
thebcitguy <wow1@my.bcit.ca>
2021-04-22 13:34:13 +0800

    import constants

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -8,7 +8,8 @@ import {
   TouchableOpacity,
   ScrollView,
 } from 'react-native';
-import styles from './styles'
+import styles from './styles';
+import { Constants } from 'expo';
 
 import {useTheme} from '@react-navigation/native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -32,13 +33,15 @@ return (
             }}>
             MY POSTED JOBS
             </Text>
+            
+            <View style={styles.container}>
+                <View style={styles.rectangle}></View>
+                {/* <Image source={require('assets/car1.png')} /> */}
+            </View>
+
         <View style={styles.card}>
             <View style={styles.cardImgWrapper}>
-                <Image
-                //source={require('../assets/car1.png')}
-                resizeMode="cover"
-                style={styles.cardImg}
-                />
+                
                 <Text style={styles.cardTitle}>ADVERT HEADING</Text>
             </View>
 
