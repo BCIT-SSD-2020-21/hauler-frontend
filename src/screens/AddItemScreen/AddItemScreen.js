07e96be27cde84dc10cb9bdcc7a1a22e77901900
abhishekpundir789 <apundir@algomau.ca>
2021-04-24 17:48:08 -0700

    update commit with adding fake images and adding rows

--- src/screens/AddItemScreen/AddItemScreen.js
+++ src/screens/AddItemScreen/AddItemScreen.js
@@ -1,5 +1,5 @@
 import React, { useState }from 'react'
-import { Text, View, TextInput, Picker, ScrollView } from 'react-native'
+import { Text, View, TextInput, Picker, ScrollView, Image } from 'react-native'
 import { StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import DropDown from 'react-native-dropdown-menu';
@@ -34,6 +34,25 @@ export default function AddItemScreen({ navigation }) {
             <View style={styles.btnContainer}>
                 <TouchableOpacity style={styles.button}><Text style={styles.btnText}>Upload Image</Text></TouchableOpacity>
             </View>
+            <View style={styles.imageRow}>
+                <View style={styles.imageColumn}>
+                <Image style={styles.image} source={{uri:'https://www.supplypost.com/Moxie/Files/HEAVY%20HAUL.jpg'}}/></View>
+                <View style={styles.imageColumn}>
+                <Image source={{uri:'https://www.logisticdynamics.com/wp-content/uploads/2017/01/eq12-1.jpeg'}}/></View>
+                <View style={styles.imageColumn}>
+                <Image source={{uri:'https://image.shutterstock.com/image-photo/big-rig-long-haul-industrial-260nw-1762531160.jpg'}}/></View>
+                </View>
+
+                <View style={styles.imageRow}>
+                    <View style={styles.imageColumn}>
+                    <Image source={{uri:'https://www.effectuation.org/wp-content/uploads/2016/08/U-Haul-1200x656.jpg'}}/></View>
+                    <View style={styles.imageColumn}>
+                    <Image source={{uri:'https://media-exp1.licdn.com/dms/image/C561BAQGeoODWyXQujA/company-background_10000/0/1576006955491?e=2159024400&v=beta&t=NxUjpgsOeQmnnesMer32t89wdFdNOAWg7qdoCvR9c6U'}}/></View>
+                    <View style = {styles.imageColumn}>
+                    <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7iNd9M5e6riVgLLME2Bid7-2C0CXeVFjZ42T9bSGM1_IhSkHTjhyiMtkbsHsD3nAOs48&usqp=CAU'}}/>
+                    </View>
+                </View>
+
             <View style={styles.btnContainer}>
                 <TouchableOpacity onPress={() => navigation.navigate('AddJunkScreen2')} style={styles.button}><Text style={styles.btnText}>Next</Text></TouchableOpacity>
             </View>
@@ -87,5 +106,20 @@ const styles = StyleSheet.create({
         fontSize: 20,
         paddingVertical: 10,
         paddingHorizontal: 50,
-    }
+    },
+    image:{
+        width: 80,
+        height: 80,
+        margin: 5,
+        resizeMode: 'contain', 
+    },
+    imageRow: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+      },
+    imageColumn: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        width: '50%',
+    },
 })
