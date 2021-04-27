aa6ae3529dabde0b4f1c8994bb613251493637ca
abhishekpundir789 <apundir@algomau.ca>
2021-04-26 20:47:22 -0700

    update commit with fixing style and value in service screen #14

--- src/screens/AddItemScreen/AddItemScreen.js
+++ src/screens/AddItemScreen/AddItemScreen.js
@@ -20,16 +20,16 @@ export default function AddItemScreen({ navigation }) {
                 <Picker.Item label="Heavy 50Kgs & above" value="heavy" />
             </Picker>
             <Picker selectedValue={selectedquantity} style={{height: 50, width: 380}} onValueChange={(itemValue, itemIndex) => setSelectedQuantity(itemValue)}>
-                <Picker.Item label="1"  />
-                <Picker.Item label="2"  />
-                <Picker.Item label="3"  />
-                <Picker.Item label="4"  />
-                <Picker.Item label="5"  />
-                <Picker.Item label="6"  />
-                <Picker.Item label="7"  />
-                <Picker.Item label="8"  />
-                <Picker.Item label="9"  />
-                <Picker.Item label="10"  />
+                <Picker.Item label="1" value="1"/>
+                <Picker.Item label="2" value="2" />
+                <Picker.Item label="3" value="3" />
+                <Picker.Item label="4" value="4" />
+                <Picker.Item label="5" value="5" />
+                <Picker.Item label="6" value="6" />
+                <Picker.Item label="7" value="7" />
+                <Picker.Item label="8" value="8" />
+                <Picker.Item label="9" value="9" />
+                <Picker.Item label="10" value="10" />
             </Picker>
             <View style={styles.btnContainer}>
                 <TouchableOpacity style={styles.button}><Text style={styles.btnText}>Upload Image</Text></TouchableOpacity>
