ee7ebbe0911e89b00808fd979bbeceabcb82d0b6
abhishekpundir789 <apundir@algomau.ca>
2021-04-28 16:53:36 -0700

    update commit with Home Screen modification #30

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,55 +1,22 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Image
-} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { Card, Button } from 'react-native-elements'
 import styles from './styles';
 
-export default function Home() {
+export default function Home({ navigation }) {
 
 return (
-    <View style={styles.cardsWrapper}>
-
-            <Text
-            style={{
-                alignSelf: 'center',
-                fontSize: 18,
-                fontWeight: 'bold',
-                color: '#333',
-            }}>
-            MY POSTED JOBS
-            </Text>
-            
-            <View style={styles.container}>
-                <View style={styles.rectangle}></View>
-                {/* <Image source={require('assets/car1.png')} /> */}
-            </View>
-
-        <View style={styles.card}>
-            <View style={styles.cardImgWrapper}>
-                
-                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-            </View>
-
-            <View style={styles.cardImgWrapper}>
-                <Image
-                //source={require('../assets/car2.png')}
-                resizeMode="cover"
-                style={styles.cardImg}
-                />
-                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-            </View>
-
-            <View style={styles.cardImgWrapper}>
-                <Image
-                //source={require('../assets/car3.png')}
-                resizeMode="cover"
-                style={styles.cardImg}
-                />
-                <Text style={styles.cardTitle}>ADVERT HEADING</Text>
-            </View>
-            
+    <View>
+        <Text> MY POSTED JOBS </Text>
+        <View style={styles.cardContainer}>
+            <Card>
+                <Image source={{ uri: 'https://www.supplypost.com/Moxie/Files/HEAVY%20HAUL.jpg'}} onPress={() => navigation.navigate('JobStatus')}/>
+                <Card.Divider />
+                <Card.Title style={styles.cardTitle}> Vancouver Haulers </Card.Title>
+                <Card.Divider />
+                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Accepted/Pending' />
+                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Edit' onPress={() => navigation.navigate('')} />
+            </Card>
         </View>
     </View>
     )
