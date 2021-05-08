0dd16fce0b256c281df3c6e0e2af5a48ad072d1b
mandeep1580 <mandeep15@gmail.com>
2021-05-08 07:06:09 -0700

    fixed image issue with passing data to mongo #62

--- src/screens/ErrandPost1/ErrandPost1.js
+++ src/screens/ErrandPost1/ErrandPost1.js
@@ -29,6 +29,7 @@ export default function ErrandPost1({ navigation }) {
           allowsEditing: true,
           aspect: [1, 1],
           quality: 1,
+          base64: true,
           //allowsMultipleSelection: true,
         });
     
