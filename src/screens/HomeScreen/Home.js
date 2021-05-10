adba2a9fefc493a3b8f667f3ea51b818d4e0d121
KulveerBrar <brarkulveer15@gmail.com>
2021-05-10 16:37:14 -0700

    Add search by location and searvice #68

--- src/screens/HomeScreen/Home.js
+++ src/screens/HomeScreen/Home.js
@@ -1,70 +1,44 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
-import { Card, Button } from 'react-native-elements'
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, View } from 'react-native';
+import SearchByService from '../../components/SearchByService/SearchByService';
 import SearchByLocation from '../../components/SearchByLocation/SearchByLocation';
-import SearchByService from '../../components/SearchByService/SearchByService'
 
 export default function Home({ navigation }) {
 
+    const [location, setLocation] = useState('')
+    const [service, setService] = useState('')
+
+    const searchService = async (value) => {
+       console.log("search pressed")
+    }
+
+    const searchLocation = async (value) => {
+        console.log("location pressed")
+    }
+
 return (
-    <ScrollView>
     <View style={styles.container}>
-        <Text> MY POSTED JOBS </Text>
-        <View style={styles.cardContainer}>
-            <Card>
-                <TouchableOpacity onPress={() => navigation.navigate('JobStatus')}>
-                <Image style={styles.cardImage} source={{ uri: 'https://www.supplypost.com/Moxie/Files/HEAVY%20HAUL.jpg'}}  /></TouchableOpacity>
-                <Card.Divider />
-                <Card.Title style={styles.cardTitle}> Vancouver Haulers </Card.Title>
-                <Card.Divider />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Accepted/Pending' />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Edit' onPress={() => navigation.navigate('AddJunkNavigator')} />
-            </Card>
+            <SearchByLocation
+                location={location}
+                setLocation={setLocation}
+                searchLocation={searchLocation}
+            />
+            <SearchByService
+                service={service}
+                setService={setService}
+                searchService={searchService}
+            />
         </View>
-        <View style={styles.cardContainer}>
-            <Card>
-                <Image style={styles.cardImage} source={{ uri: 'https://threebestrated.ca/images/SmallMoves-Vancouver-BC.jpeg'}} />
-                <Card.Divider />
-                <Card.Title style={styles.cardTitle}> Surrey Movers </Card.Title>
-                <Card.Divider />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Accepted/Pending' />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Edit' />
-            </Card>
-        </View>
-        <View style={styles.cardContainer}>
-            <Card>
-                <Image style={styles.cardImage} source={{ uri: 'https://yocoman.com/wp-content/uploads/2019/05/earned-services.jpg'}} />
-                <Card.Divider />
-                <Card.Title style={styles.cardTitle}> Vancouver Errand </Card.Title>
-                <Card.Divider />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Accepted/Pending' />
-                <Button buttonStyle={{ borderRadius: 5, backgroundColor: '#16B3D5', marginTop: 10 }} title='Edit' />
-            </Card>
-        </View>
-    </View>
-    </ScrollView>
     )
 };
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    cardContainer: {
-        width: '100%'
-    },
-    cardText: {
-        marginTop: 10,
-        width: 300,
-        textAlign: 'center'
-    },
-    cardTitle: {
-        color: '#2EBCAC'
-    },
-    cardImage: {
-        width: 250,
-        height: 150,
-        alignSelf: 'center'
-    },
+        backgroundColor: 'white',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        width: '100%',
+        height: '100%'
+    }
 })
