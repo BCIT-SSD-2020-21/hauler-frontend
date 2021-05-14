212ea54fd2a4428fd3f1a219f520e9bffcd3e67d
KulveerBrar <brarkulveer15@gmail.com>
2021-05-13 20:51:30 -0700

    Refactor errand summary screen to add edit fn #84

--- src/screens/ErrandSummary/ErrandSummary.js
+++ src/screens/ErrandSummary/ErrandSummary.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useRef } from 'react'
-import { Text, View, Image, Dimensions, StyleSheet } from 'react-native'
+import { Text, View, Dimensions, StyleSheet } from 'react-native'
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import { Context } from '../../context/ContextProvider';
-import { postItem } from '../../../network';
+import { postItem, updateOnePost } from '../../../network';
 import PostInfo from '../../components/PostInfo/PostInfo'
 import MapView from 'react-native-maps';
 import MapViewDirections from 'react-native-maps-directions';
@@ -11,16 +11,11 @@ import { GOOGLE_MAP_API } from '@env';
 export default function ErrandSummary({ navigation, route }) {
     const { width, height } = Dimensions.get('window');
     const mapView = useRef();
-    const { image, selectedweight, selectedquantity, postHeading, description, pickUpAddress, 
-        dropOffAddress, pickContactPerson, pickUpPhoneNumber, pickUpSpecialInstructions, 
-        dropOffContactPerson, dropOffPhoneNumber, dropOffSpecialInstructions, sliderValue, service } = route.params;
+    const { image, selectedweight, selectedquantity, postHeading, description, pickUpAddress,
+        dropOffAddress, pickContactPerson, pickUpPhoneNumber, pickUpSpecialInstructions,
+        dropOffContactPerson, dropOffPhoneNumber, dropOffSpecialInstructions, sliderValue, service, dropOffCity, dropOffAddressLat, dropOffAddressLng, operation, postId, pickUpCity, pickUpAddressLat, pickUpAddressLng } = route.params;
 
     const { currentUser } = useContext(Context)
-
-    const pickUpAddressLat = pickUpAddress.geometry.location.lat
-    const pickUpAddressLng = pickUpAddress.geometry.location.lng
-    const dropOffAddressLat = dropOffAddress.geometry.location.lat
-    const dropOffAddressLng = dropOffAddress.geometry.location.lng
 
     const [distance, setDistance] = useState('')
     const [duration, setDuration] = useState('')
@@ -34,10 +29,11 @@ export default function ErrandSummary({ navigation, route }) {
             longitude: dropOffAddressLng,
         }
     ])
+
     return (
         <ScrollView>
             <View style={styles.container}>
-                <PostInfo 
+                <PostInfo
                     postHeading={postHeading}
                     description={description}
                     selectedweight={selectedweight}
@@ -47,51 +43,50 @@ export default function ErrandSummary({ navigation, route }) {
                     pickContactPerson={pickContactPerson}
                     pickUpPhoneNumber={pickUpPhoneNumber}
                     pickUpSpecialInstructions={pickUpSpecialInstructions}
-                    dropOffAddress= {dropOffAddress}
+                    dropOffAddress={dropOffAddress}
                     dropOffContactPerson={dropOffContactPerson}
                     dropOffContactNumber={dropOffPhoneNumber}
                     sliderValue={sliderValue}
                     distance={distance}
                     duration={duration}
-                 />
-                    <MapView
-                        style={styles.map}
-                        ref={mapView}
-                    >
-                        {coordinates.map((coordinate, index) =>
-                            <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate}
-                            />
-                        )}
-                        <MapViewDirections
-                            apikey={GOOGLE_MAP_API}
-                            origin={coordinates[0]}
-                            waypoints={coordinates}
-                            destination={coordinates[coordinates.length - 1]}
-                            strokeWidth={3}
-                            strokeColor='#DE0303'
-                            optimizeWaypoints={true}
-                            onReady={result => {
-                                setDistance(result.distance)
-                                setDuration(result.duration)
-                                mapView.current.fitToCoordinates(result.coordinates, {
-                                    edgePadding: {
-                                        right: (width / 20),
-                                        bottom: (height / 20),
-                                        left: (width / 20),
-                                        top: (height / 20),
-                                    }
-                                }
-                                );
-                            }}
-                            onError={(errorMessage) => {
-                                console.log(`Error: ${errorMessage}`);
-                            }}
+                />
+                <MapView
+                    style={styles.map}
+                    ref={mapView}
+                >
+                    {coordinates.map((coordinate, index) =>
+                        <MapView.Marker key={`coordinate_${index}`} coordinate={coordinate}
                         />
-                    </MapView>
-                </View>
-                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ErrandPost1')}><Text style={styles.buttonTitle}> Edit </Text></TouchableOpacity>
-                    
-                    <View>
+                    )}
+                    <MapViewDirections
+                        apikey={GOOGLE_MAP_API}
+                        origin={coordinates[0]}
+                        waypoints={coordinates}
+                        destination={coordinates[coordinates.length - 1]}
+                        strokeWidth={3}
+                        strokeColor='#DE0303'
+                        optimizeWaypoints={true}
+                        onReady={result => {
+                            setDistance(result.distance)
+                            setDuration(result.duration)
+                            mapView.current.fitToCoordinates(result.coordinates, {
+                                edgePadding: {
+                                    right: (width / 20),
+                                    bottom: (height / 20),
+                                    left: (width / 20),
+                                    top: (height / 20),
+                                }
+                            }
+                            );
+                        }}
+                        onError={(errorMessage) => {
+                            console.log(`Error: ${errorMessage}`);
+                        }}
+                    />
+                </MapView>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('ErrandPost1')}><Text style={styles.buttonTitle}> Edit </Text></TouchableOpacity>
+
+                {operation === "create" ?
                     <TouchableOpacity style={styles.button} onPress={async () => {
                         await postItem(
                             currentUser.uid,
@@ -102,15 +97,15 @@ export default function ErrandSummary({ navigation, route }) {
                             selectedquantity,
                             image,
                             sliderValue,
-                            pickUpAddress.formatted_address,
-                            pickUpAddress.vicinity,
+                            pickUpAddress,
+                            pickUpCity,
                             pickUpAddressLat,
                             pickUpAddressLng,
                             pickContactPerson,
                             pickUpPhoneNumber,
                             pickUpSpecialInstructions,
-                            dropOffAddress.formatted_address,
-                            dropOffAddress.vicinity,
+                            dropOffAddress,
+                            dropOffCity,
                             dropOffAddressLat,
                             dropOffAddressLng,
                             dropOffContactPerson,
@@ -119,7 +114,36 @@ export default function ErrandSummary({ navigation, route }) {
                             distance
                         ); navigation.navigate('Confirmation')
                     }}><Text style={styles.buttonTitle}> Post the Job </Text></TouchableOpacity>
-                    </View>
+                    :
+                    <TouchableOpacity style={styles.button}
+                        onPress={async () => {
+                            await updateOnePost(
+                                postId,
+                                // service,
+                                postHeading,
+                                description,
+                                selectedweight,
+                                selectedquantity,
+                                // imageUrl,
+                                sliderValue,
+                                pickUpAddress,
+                                pickUpCity,
+                                pickUpAddressLat,
+                                pickUpAddressLng,
+                                pickContactPerson,
+                                pickUpPhoneNumber,
+                                pickUpSpecialInstructions,
+                                dropOffAddress,
+                                dropOffCity,
+                                dropOffAddressLat,
+                                dropOffAddressLng,
+                                dropOffContactPerson,
+                                dropOffPhoneNumber,
+                                dropOffSpecialInstructions,
+                                distance,
+                            ); navigation.navigate('Confirmation')
+                        }}><Text style={styles.buttonTitle}>Submit Edited Post</Text></TouchableOpacity>}
+            </View>
         </ScrollView>
     )
 }
