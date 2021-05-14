f9856dea94e6a921ee8bf8823f7fbad54ff071b4
abhishekpundir789 <apundir@algomau.ca>
2021-05-13 21:57:10 -0700

    update commit with fixed scroll on profile #81

--- src/screens/ProfileScreen/Profile1.js
+++ src/screens/ProfileScreen/Profile1.js
@@ -231,7 +231,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: 'center',
-        marginVertical: 20,
+        // marginVertical: 20,
         width: '100%',
         backgroundColor: 'white'
     },
