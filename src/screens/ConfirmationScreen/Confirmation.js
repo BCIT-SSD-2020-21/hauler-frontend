dd370022330002aa06145fae7169c1a6f1722d60
abhishekpundir789 <apundir@algomau.ca>
2021-05-13 21:25:24 -0700

    update commit #81

--- src/screens/ConfirmationScreen/Confirmation.js
+++ src/screens/ConfirmationScreen/Confirmation.js
@@ -7,43 +7,57 @@ export default function Confirmation({ navigation }) {
     return (
         <ScrollView>
             <View style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image source={require('../../../assets/IMG-20210504-WA0000.jpg')} style={styles.imageDisplay}/>
+                </View>
                 <Text style={styles.text}>Successfully Posted !!</Text>
                 <Text style={styles.text}>Thank You!</Text>
                 <Text style={styles.text1}>Your job has been posted</Text>
             </View>
-            <View style={styles.btnContainer}>
-                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}><Text style={styles.btnText}>Return To My Job List </Text></TouchableOpacity>
-            </View>
+            
+                <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}><Text style={styles.buttonTitle}>Return To My Job List </Text></TouchableOpacity>
         </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: 'white',
         flex: 1,
-        alignItems: 'center',
-        marginTop: 150,
+        width: '100%',
+        height: '100%',
+        paddingVertical: 10
     },
     text: {
         fontWeight: 'bold',
         alignItems: 'center',
         fontSize: 30,
     },
-    btnContainer: {
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 30,
-    },
     button: {
         backgroundColor: '#0177FC',
-        borderRadius: 10,
-        display: 'flex',
+        marginLeft: 30,
+        marginRight: 30,
+        marginTop: 20,
+        height: 48,
+        borderRadius: 20,
+        alignItems: "center",
+        justifyContent: 'center'
     },
-    btnText: {
+    buttonTitle: {
         color: 'white',
-        fontSize: 20,
-        paddingVertical: 10,
-        paddingHorizontal: 50,
+        fontSize: 16,
+        fontWeight: "bold"
+    },
+    imageContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageDisplay:{
+        width: 200,
+        height: 200,
+        marginLeft: 50,
+        marginTop: 5,
+        resizeMode: 'contain', 
     },
 })
